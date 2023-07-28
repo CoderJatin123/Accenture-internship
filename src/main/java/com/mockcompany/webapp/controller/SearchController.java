4d050126e9a5ad20edf21b5c9e61e53ea8b8646a
Jatin <coderjatin123@gmail.com>
2023-07-28 13:13:50 +0530

    Initial commit

---
+++ src/main/java/com/mockcompany/webapp/controller/SearchController.java
@@ -0,0 +1,60 @@
+/*
+ * Java classes are grouped in "packages". This allows them to be referenced and used in other
+ * classes using import statements.  Any class in this project is prefixed in the com.mockcompany.webapp
+ * package.
+ *
+ *   https://www.w3schools.com/java/java_packages.asp
+ *
+ * For general help with Java, see the tutorialspoint tutorial:
+ *
+ *   https://www.tutorialspoint.com/java/index.htm
+ */
+package com.mockcompany.webapp.controller;
+
+/*
+ * An import statement allows the current class to use the class being imported
+ */
+import com.mockcompany.webapp.data.ProductItemRepository;
+import com.mockcompany.webapp.model.ProductItem;
+/* The springframework package allows us to take advantage of the spring capabilities */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/* java.util package provides useful utilities */
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@RestController
+public class SearchController {
+
+    private final ProductItemRepository productItemRepository;
+
+    @Autowired
+    public SearchController(ProductItemRepository productItemRepository) {
+        this.productItemRepository = productItemRepository;
+    }
+
+    @GetMapping("/api/products/search")
+    public Collection<ProductItem> search(@RequestParam("query") String query) {
+
+        Iterable<ProductItem> allItems = this.productItemRepository.findAll();
+        List<ProductItem> itemList = new ArrayList<>();
+
+        // This is a loop that the code inside will execute on each of the items from the database.
+        for (ProductItem item : allItems) {
+
+            // TODO: Figure out if the item should be returned based on the query parameter!
+
+            //My work is here :)
+            boolean matchesSearch = item.getName().toLowerCase().contains(query.toLowerCase()) || item.getDescription().toLowerCase().contains(query.toLowerCase());
+
+            if(matchesSearch){
+            itemList.add(item);
+            }
+        }
+        return itemList;
+    }
+}
